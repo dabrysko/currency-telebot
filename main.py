3ec67f6b160211660ee867340651af799df8fa63
dabrysko <103834296+dabrysko@users.noreply.github.com>
2023-02-13 17:55:44 +0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,71 @@
+import telebot
+from telebot import types
+import requests
+
+token = "6046868188:AAHL8FXtYctRN_CngG0B6RDgPaxasCC0_70"
+
+bot = telebot.TeleBot(token)
+message_to_cryptocurrency_name={'BTC': 'bitcoin', 'BNB': 'binancecoin', 'USDC': 'usd-coin', 'ETH': 'ethereum', 'USDT': 'tether'}
+
+
+def get_cryptocurrency_exchange_rate():
+    cryptocurrency = ['bitcoin', 'ethereum', 'tether', 'binancecoin', 'usd-coin']
+    res = {}
+    for c in cryptocurrency:
+        res[c] = '$' + str(
+            requests.get(f'https://api.coingecko.com/api/v3/coins/{c}/tickers').json()['tickers'][0]['last'])
+
+    return res
+
+
+@bot.message_handler(['start'])
+def start(message):
+    markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+    butt1 = types.KeyboardButton("Валюты!")
+    butt2 = types.KeyboardButton("Помощь!")
+    markup.add(butt1, butt2)
+    bot.send_message(message.chat.id, text='Привет', reply_markup=markup)
+
+
+@bot.message_handler(content_types=['text'])
+def func(message):
+    if message.text == "Валюты!":
+        markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+        but1 = types.KeyboardButton("BTC")
+        but2 = types.KeyboardButton("ETH")
+        but3 = types.KeyboardButton("USDT")
+        but4 = types.KeyboardButton("BNB")
+        but5 = types.KeyboardButton("USDC")
+        back = types.KeyboardButton("Вернуться в главное меню")
+        markup.add(but1, but2, but3, but4, but5, back)
+        bot.send_message(message.chat.id, text="Все валюты", reply_markup=markup)
+
+
+    elif message.text == "Помощь!":
+        bot.send_message(message.chat.id, text="Выбери валюту из интересующих во вкладке валюты, как нажмешь тебе высветится ее стоимость")
+
+
+    elif message.text == "Вернуться в главное меню":
+        bot.send_message(message.chat.id, text="Вы вернулись в меню")
+        start(message)
+
+    elif message.text in ('BTC', 'BNB', 'USDC', 'ETH', 'USDT'):
+        name = message.text
+        res = get_cryptocurrency_exchange_rate()[message_to_cryptocurrency_name[name]]
+        bot.send_message(message.chat.id, f'курс {name}: {res}')
+
+
+
+bot.infinity_polling()
+
+
+
+
+
+
+
+
+
+
+
+
